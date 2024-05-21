a0d5bc9ff10ba3cec0c65315187df90c9f3bfc41
Jackson Roberts <jacksonroberts25@gmail.com>
2024-05-21 14:22:50 -0600

    .

--- components/under-construction.tsx
+++ components/under-construction.tsx
@@ -1,5 +1,5 @@
 import { Callout } from 'nextra/components'
 
 export const UnderConstruction = () => (
-  <Callout type='info'>This page is under construction.</Callout>
+  <Callout type='info'>This page is under construction. More information coming soon! If you have any questions, please ask them in our <u><a href="https://discord.gg/nwXFvtJ92g">Discord server</a></u> and we'll answer them promptly! 😊</Callout>
 )
--- pages/asset-creator/_meta.tsx
+++ pages/asset-creator/_meta.tsx
@@ -1,6 +1,5 @@
 export default {
   characters: 'Characters',
-  platforms: 'Platforms',
   props: 'Props',
   scenery: 'Create from a Sketch',
 }
