957d0a4e72e6a59e70ca714635c2640fc27b86b4
Jackson Roberts <jacksonroberts25@gmail.com>
2024-05-30 03:38:39 -0600

    update tutorial.

--- components/under-construction.tsx
+++ components/under-construction.tsx
@@ -1,6 +1,6 @@
 import { Callout } from 'nextra/components'
 
 export const UnderConstruction = () => (
-  <Callout type='info'>Our docs are always improving. Expect more information coming to this page soon!<br></br>
+  <Callout type='info'>This docs page is under construction. Expect more information coming to this page soon!<br></br>
   If you have questions, please ask them in our <u><a href="https://discord.gg/nwXFvtJ92g">Discord server</a></u> and we'll answer them promptly! 😊</Callout>
 )
