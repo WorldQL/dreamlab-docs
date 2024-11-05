1f0a1a6aadd26839dcd4a373184521d8c92d24b9
Jackson Roberts <jacksonroberts25@gmail.com>
2024-11-05 15:12:35 -0700

    .

---
+++ docs/thanks.md
@@ -0,0 +1,10 @@
+---
+sidebar_position: 999
+---
+
+# Special Thanks
+
+Dreamlab is nothing without its users. Thanks to the following for helping us improve the documentation:
+
+- 42ultra
+- Voodooc
--- src/theme/DocCard/index.tsx
+++ src/theme/DocCard/index.tsx
@@ -1,51 +1,52 @@
-import React, {type ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
+import React, { type ReactNode } from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
 import {
   useDocById,
   findFirstSidebarItemLink,
-} from '@docusaurus/plugin-content-docs/client';
-import {usePluralForm} from '@docusaurus/theme-common';
-import isInternalUrl from '@docusaurus/isInternalUrl';
-import {translate} from '@docusaurus/Translate';
+} from "@docusaurus/plugin-content-docs/client";
+import { usePluralForm } from "@docusaurus/theme-common";
+import isInternalUrl from "@docusaurus/isInternalUrl";
+import { translate } from "@docusaurus/Translate";
 
-import type {Props} from '@theme/DocCard';
-import Heading from '@theme/Heading';
+import type { Props } from "@theme/DocCard";
+import Heading from "@theme/Heading";
 import type {
   PropSidebarItemCategory,
   PropSidebarItemLink,
-} from '@docusaurus/plugin-content-docs';
+} from "@docusaurus/plugin-content-docs";
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 function useCategoryItemsPlural() {
-  const {selectMessage} = usePluralForm();
+  const { selectMessage } = usePluralForm();
   return (count: number) =>
     selectMessage(
       count,
       translate(
         {
-          message: '1 item|{count} items',
-          id: 'theme.docs.DocCard.categoryDescription.plurals',
+          message: "1 item|{count} items",
+          id: "theme.docs.DocCard.categoryDescription.plurals",
           description:
-            'The default description for a category card in the generated index about how many items this category includes',
+            "The default description for a category card in the generated index about how many items this category includes",
         },
-        {count},
-      ),
+        { count }
+      )
     );
 }
 
 function CardContainer({
   href,
   children,
+  classes = "padding--lg",
 }: {
   href: string;
   children: ReactNode;
+  classes?: string;
 }): JSX.Element {
   return (
-    <Link
-      href={href}
-      className={clsx('card padding--lg', styles.cardContainer)}>
+    <Link href={href} className={clsx("card", styles.cardContainer, classes)}>
       {children}
     </Link>
   );
@@ -56,24 +57,39 @@ function CardLayout({
   icon,
   title,
   description,
+  image,
 }: {
   href: string;
   icon: ReactNode;
   title: string;
   description?: string;
+  image?: string | undefined;
 }): JSX.Element {
   return (
-    <CardContainer href={href}>
+    <CardContainer href={href} classes={image ? "padding--md" : undefined}>
+      {image && (
+        <img
+          src={useBaseUrl(image)}
+          style={{
+            maxHeight: "200px",
+            width: "auto",
+            display: "block",
+            margin: "0 auto 1rem",
+          }}
+        />
+      )}{" "}
       <Heading
         as="h2"
-        className={clsx('text--truncate', styles.cardTitle)}
-        title={title}>
+        className={clsx("text--truncate", styles.cardTitle)}
+        title={title}
+      >
         {icon} {title}
       </Heading>
       {description && (
         <p
-          className={clsx('text--truncate', styles.cardDescription)}
-          title={description}>
+          className={clsx("text--truncate", styles.cardDescription)}
+          title={description}
+        >
           {description}
         </p>
       )}
@@ -104,24 +120,29 @@ function CardCategory({
   );
 }
 
-function CardLink({item}: {item: PropSidebarItemLink}): JSX.Element {
-  const icon = isInternalUrl(item.href) ? '📄️' : '🔗';
+function CardLink({ item }: { item: PropSidebarItemLink }): JSX.Element {
+  let icon = isInternalUrl(item.href) ? "📄️" : "🔗";
   const doc = useDocById(item.docId ?? undefined);
+  const image: string | undefined =
+    (item.customProps?.cardImage as string) ?? undefined;
+
+  if (image) icon = undefined;
   return (
     <CardLayout
       href={item.href}
       icon={icon}
       title={item.label}
       description={item.description ?? doc?.description}
+      image={image}
     />
   );
 }
 
-export default function DocCard({item}: Props): JSX.Element {
+export default function DocCard({ item }: Props): JSX.Element {
   switch (item.type) {
-    case 'link':
+    case "link":
       return <CardLink item={item} />;
-    case 'category':
+    case "category":
       return <CardCategory item={item} />;
     default:
       throw new Error(`unknown item type ${JSON.stringify(item)}`);
